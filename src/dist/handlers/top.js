a8c3a42f008fd6b9edf83caaff0e2950bd76ebb5
Gen <genya.t.6313@gmail.com>
2023-07-07 22:24:43 +0900

    top-handler: dbへアクセスしてデータを取得し、レンダリング時の変数に渡す処理を作成

--- src/dist/handlers/top.js
+++ src/dist/handlers/top.js
@@ -1,39 +1,21 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.renderTopPage = void 0;
-const models_1 = __importDefault(require("../models"));
-// select
-const tst = () => __awaiter(void 0, void 0, void 0, function* () {
-    const rows = yield models_1.default.UserFavoriteShops.findAll({
-        attributes: ["user_id", "shop_category"],
-    });
-    rows.forEach((row) => {
-        const user_id = row.user_id;
-        const shop_category = row.shop_category;
-        console.log(user_id, shop_category);
-        // return { user_id, shop_category };
-    });
-});
-tst;
-console.log(tst);
-// });
-// console.log(userFavoriteShops.user_id);
-// const categories = userFavoriteShops;
-const categories = ["飲食", "美容"];
-// const username = [""];
+const index_1 = __importDefault(require("../models/index"));
 const renderTopPage = (req, res) => {
-    res.render("top", { categories: categories });
+    // UserFavoriteShopsデータベースのshop_categoryデータを取得
+    index_1.default.UserFavoriteShops.findAll({ attributes: ["shop_category"] }).then((data) => {
+        // 取得データ(オブジェクト)の値を配列に格納した後に、重複を削除
+        const dataValues = [];
+        data.forEach((dataValue) => {
+            dataValues.push(dataValue.shop_category);
+        });
+        const setedDataValues = Array.from(new Set(dataValues));
+        // top.ejsをレンダリング
+        res.render("top.ejs", { shopCategories: setedDataValues });
+    });
 };
 exports.renderTopPage = renderTopPage;
