e6aaadd198b567b711a01bf629645e1bb4488027
Gen <genya.t.6313@gmail.com>
2023-07-16 19:06:35 +0900

    created empty post check function

--- src/routes/list.ts
+++ src/routes/list.ts
@@ -3,10 +3,24 @@ import {
   getSelectedCategory,
   renderListPage,
   renderCreateListPage,
+  checkPostedNewList,
+  createList,
+  deleteList,
+  renderEditListPage,
+  updateList,
+  checkPostedUpdateList,
 } from "../handlers/list";
 
 router.get("/:index", getSelectedCategory, renderListPage);
-router.post("/:index");
+router.post("/:index", getSelectedCategory, checkPostedNewList, createList);
 router.get("/:index/create", getSelectedCategory, renderCreateListPage);
+router.post("/:index/:id", deleteList);
+router.get("/:index/edit/:id", getSelectedCategory, renderEditListPage);
+router.post(
+  "/:index/update/:id",
+  getSelectedCategory,
+  checkPostedUpdateList,
+  updateList
+);
 
 module.exports = router;
