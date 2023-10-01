c1dd94a63abb22e587805ec43abd0c63aeacbdc0
Gen <genya.t.6313@gmail.com>
2023-10-01 22:14:03 +0900

    a

--- src/dist/handlers/checkAuthenticated.js
+++ src/dist/handlers/checkAuthenticated.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.checkAuthenticated = void 0;
-// サインイン状態を判断するミドルウェア
+// サインイン状態を判断
 const checkAuthenticated = (req, res, next) => {
     // passport認証を通過(サインイン)している場合はtrueを返す
     if (req.isAuthenticated()) {
--- src/handlers/checkAuthenticated.ts
+++ src/handlers/checkAuthenticated.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
-// サインイン状態を判断するミドルウェア
+// サインイン状態を判断
 export const checkAuthenticated = (
   req: Request,
   res: Response,
